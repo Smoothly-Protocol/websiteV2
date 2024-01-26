b77ad7c3b1e190bb058c1cd30744ea0d1037e94d
noah figueras <noahfigueras@gmail.com>
2024-01-26 11:45:26 +0100

    Loader modal centered on mobile

--- app/routes/components/Loader.tsx
+++ app/routes/components/Loader.tsx
@@ -16,7 +16,9 @@ export const Loader = (props: {hash}) => {
   return (
     <div className="modal fade align-items-center" id="loaderModal" data-bs-backdrop="static" data-bs-keyboard="false" data-bs-target="#loaderModal" tabIndex="-1" aria-labelledby="loaderModalLabel" aria-hidden="true">
       <div className="modal-dialog  modal-dialog-centered">
-        <lottie-player src="img/Smoothly.json" background="transparent"  speed="1"  style={{width: "300px", height: "300px", marginLeft: "5vw"}} loop autoplay></lottie-player>
+        <div className="modal-content align-items-center" style={{ border: "none", background: "transparent !important"}}>
+          <lottie-player src="img/Smoothly.json" background="transparent"  speed="1"  style={{width: "300px", height: "300px"}} loop autoplay></lottie-player>
+        </div>
       </div>
     </div>
   );
